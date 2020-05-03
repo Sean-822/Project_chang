2e18a73d6eb0a11b30d8fb3e8e1164ec6b67d960
rodneywhite <rwhite33@illinois.edu>
2020-05-03 10:01:43 -0500

    Preliminary interface for project.

---
+++ app/src/main/java/com/example/dimensionalanalysis/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.dimensionalanalysis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    //Initial class declarations
+    EditText editText;
+    TextView textView, textView2, textView3, valueininches;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //First text view
+        textView = findViewById(R.id.textView);
+        //Second text view
+        textView2 = findViewById(R.id.textView2);
+        //Third text view
+        textView3 = findViewById(R.id.textView3);
+        //Fourth text view
+        valueininches = findViewById(R.id.valueOfInches);
+
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+
+        //Adding a new activity
+        button.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                convertFromMMtoIn();
+
+            }
+        }));
+
+
+
+    }
+
+    private void convertFromMMtoIn() {
+        String valueEntered = editText.getText().toString();
+
+        double Mili = Double.parseDouble(valueEntered);
+
+        double inches = Mili * 0.0393701;
+        valueininches.setText("" + inches);
+    }
+}
